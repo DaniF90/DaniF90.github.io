69ff71cffad8a35347b9b16f4352616302d4b61e
DaniF90 <127355642+DaniF90@users.noreply.github.com>
2023-03-21 23:02:23 -0300

    Add files via upload

---
+++ function2.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_color').addEventListener('click',function(){
+    document.body.style.backgroundColor = '#000';
+})
+
+document.getElementById('boton_default').addEventListener('click',function(){
+    document.body.style.backgroundColor = '#ADD8E6';
+})
